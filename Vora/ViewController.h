887e67623c6cadb661e219b901d2dac063281ee9
Erik Griffin <Erik@Eriks-MacBook-Air.local>
2014-12-16 21:45:35 -0500

    Initial Commit

---
+++ Vora/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Vora
+//
+//  Created by Erik Griffin on 12/16/14.
+//  Copyright (c) 2014 Erik Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Vora/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Vora
+//
+//  Created by Erik Griffin on 12/16/14.
+//  Copyright (c) 2014 Erik Griffin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

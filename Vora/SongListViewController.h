c28578e8decf6bc09ff81d2cd987d66b095293ec
Erik Griffin <Erik@Eriks-MacBook-Air.local>
2014-12-19 14:10:54 -0500

    Added blank SongListViewController

---
+++ Vora/SongListViewController.h
@@ -0,0 +1,13 @@
+//
+//  SongListViewController.h
+//  Vora
+//
+//  Created by Erik Griffin on 12/19/14.
+//  Copyright (c) 2014 Erik Griffin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface SongListViewController : ViewController
+
+@end
---
+++ Vora/SongListViewController.m
@@ -0,0 +1,37 @@
+//
+//  SongListViewController.m
+//  Vora
+//
+//  Created by Erik Griffin on 12/19/14.
+//  Copyright (c) 2014 Erik Griffin. All rights reserved.
+//
+
+#import "SongListViewController.h"
+
+@interface SongListViewController ()
+
+@end
+
+@implementation SongListViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
